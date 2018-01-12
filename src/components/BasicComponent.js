bd1e9f06d8a0d0b7dfa73d3c5b0089e6d37b92ab
Pushpak.Waghmare <pushpak.waghmare@harbingergroup.com>
2018-01-12 12:17:47 +0530

    added button event handling code

--- src/components/BasicComponent.js
+++ src/components/BasicComponent.js
@@ -4,22 +4,21 @@ class BasicComponent extends React.Component{
     constructor(props){
         super(props);
 
-        this.state = { placeholder:'Enter Something' , text:'' }
+        this.state = { placeholder:'Enter Something' , text:'' ,divText : ''}
     }
 
 
     onEnterClick (event) {
-        console.log(this.state, this.state.text);      
-        //alert(event);
+        this.setState({divText : this.state.text});
     }  
     
 
     render(){
         return (
             <div>
-                <input placeholder={this.state.placeholder} onChange={event => this.setState({ text: event.target.value })} />
-                <input className="defaultButton" value="Enter" type="button" onClick={this.onEnterClick()} />
-                <div>{this.state.text}</div>
+                <input placeholder={this.state.placeholder} value={this.state.text} onChange={event => this.setState({ text: event.target.value })} />
+                <input className="defaultButton" value="Enter" type="button" onClick={this.onEnterClick.bind(this)} />
+                <div>{this.state.divText}</div>
             </div>
         );
     }
