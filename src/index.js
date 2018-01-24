9e3730d21260fa6aaa16176d8926ed987bff5fb3
Pushpak.Waghmare <pushpak.waghmare@harbingergroup.com>
2018-01-24 12:35:41 +0530

    a

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import counter from './reducers'
 const store = createStore(counter)
 const rootEl = document.getElementById('root')
 
-const render = () => ReactDOM.render(
+const render = () => ReactDOM.render (
   <Counter
     value={store.getState()}
     onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
